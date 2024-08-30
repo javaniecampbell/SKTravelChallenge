37624703c44cdddcdb57dee6b06c06c6ae301fe8
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-08-30 15:38:32 -0400

    feat: adds ability to create kernel
    Completes the 'Build a Kernel' section of the "Develop AI agents using Azure OpenAI and the Semantic Kernel SDK Challenge" challenge.

--- SkTravelChallenge.Console/Program.cs
+++ SkTravelChallenge.Console/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+
+// Create kernel
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(
+    deploymentName: "<deployment-name>", // Azure OpenAI deployment name
+    endpoint: "<aoai-endpoint-url>", // Azure OpenAI endpoint
+    apiKey: "<aoai-key>", // Azure OpenAI API key
+    modelId: "gpt-35-turbo-16k" // Azure OpenAI model ID
+);
+
+var kernel = builder.Build();
+
+var result = await kernel.InvokePromptAsync(
+        "Give me a list of breakfast foods with eggs and cheese");
+Console.WriteLine(result);
+
+
